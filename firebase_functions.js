1199d7bec3c01970effb988bf14944194a7eb917
mkcarl <munkye29@gmail.com>
2023-01-31 15:58:41 +0800

    local processing with processing.js

--- firebase_functions.js
+++ firebase_functions.js
@@ -12,14 +12,52 @@ admin.initializeApp({
 const db = getFirestore();
 const firebaseBucket = admin.storage().bucket()
 
-exports.uploadToFirebase = async (key) => {
-    [pathName, file] = key.split('/')
+// exports.uploadToFirebase = async (key) => {
+//     [pathName, file] = key.split('/')
+//     console.log("Starting upload");
+//     const allUpload = [];
+//     for (const frame of await readdir(`${tempFile}/output`)) {
+//         const upload = firebaseBucket.upload(`${tempFile}/output/${frame}`, {
+//             public: true,
+//             destination: `uncropped/${pathName}/${frame}`,
+//             metadata: {
+//                 metadata: {
+//                     firebaseStorageDownloadTokens:randomUUID()
+//
+//                 }
+//             }
+//         })
+//         allUpload.push(upload)
+//     }
+//     for (const partUpload of _.chunk(allUpload, 15)) {
+//         console.log(`Uploading ${partUpload.length} files`)
+//         await Promise.all(partUpload)
+//     }
+//     console.log("Done upload")
+// }
+
+exports.recordInFirestore = async (videoId) => {
+    const data = {
+        video_id: videoId
+    }
+
+    await db.collection('available_video').doc().set(data)
+}
+
+exports.recordExistInFirestore = async (videoId) => {
+    const doc = await db.collection('available_video').where('video_id', '==', videoId).get()
+    return !doc.empty
+}
+
+exports.uploadToFirebase = async (folder, videoId)=> {
     console.log("Starting upload");
-    const allUpload = [];
-    for (const frame of await readdir(`${tempFile}/output`)) {
-        const upload = firebaseBucket.upload(`${tempFile}/output/${frame}`, {
+
+    const files = (await readdir(folder)).slice(1)
+    const allUpload = []
+    for (const file of files){
+        const upload = firebaseBucket.upload(`${folder}/${file}`, {
             public: true,
-            destination: `uncropped/${pathName}/${frame}`,
+            destination: `uncropped/${videoId}/${file}`,
             metadata: {
                 metadata: {
                     firebaseStorageDownloadTokens:randomUUID()
@@ -31,15 +69,12 @@ exports.uploadToFirebase = async (key) => {
     }
     for (const partUpload of _.chunk(allUpload, 15)) {
         console.log(`Uploading ${partUpload.length} files`)
-        await Promise.all(partUpload)
+        try {
+            await Promise.all(partUpload)
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
     console.log("Done upload")
-}
-
-exports.recordInFirestore = async (videoId) => {
-    const data = {
-        video_id: videoId
-    }
-
-    await db.collection('available_video').doc().set(data)
 }
---
+++ processing.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const {execSync} = require("child_process")
+const ffmpeg = require("fluent-ffmpeg");
+const {uploadToFirebase, recordInFirestore, recordExistInFirestore} = require("./firebase_functions");
+require("dotenv").config()
+
+
+
+const path = 'temp'
+
+async function downloadFromYoutube(info) {
+    if (!fs.existsSync(path)) fs.mkdirSync(path)
+    console.log("Downloading from YouTube")
+
+    if (fs.existsSync(`${path}/${info.videoDetails.videoId}.mp4`)) {
+        console.log("File already downloaded")
+        return
+    }
+    await new Promise((resolve) => { // wait
+        ytdl(info.videoDetails.video_url, {quality: 134})
+            .pipe(fs.createWriteStream(`${path}/${info.videoDetails.videoId}.mp4`))
+            .on('close', () => {
+                resolve(); // finish
+            })
+    })
+    console.log("Done downloading")
+}
+
+async function segmentVideo(info){
+    const videoId = info.videoDetails.videoId
+    const videoLength = +info.videoDetails.lengthSeconds
+    if (!fs.existsSync(`${path}/${videoId}`)) fs.mkdirSync(`${path}/${videoId}`)
+    const secondsPerSegment = 60
+
+    const command = `ffmpeg -i "${path}/${videoId}.mp4"  -f segment -segment_time ${secondsPerSegment} -vcodec copy -reset_timestamps 1 -map 0:0 -an ${path}/${videoId}/segment%02d.mp4`
+
+    console.log("Extracting frames")
+    execSync(command)
+    console.log("Done extracting")
+
+
+}
+
+async function frames(info){
+    const videoId = info.videoDetails.videoId
+    const vidPath = `${path}/${videoId}`
+    const outputPath = `${path}/${videoId}/output`
+    if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath)
+
+    const files = fs.readdirSync(vidPath)
+    // for each file in files
+    // ffmpeg -i ${tempFile}/\${file}.mp4 -r 1 -qscale:v 2 ${tempFile}/output/\${file}-frame%03d.jpg
+    for (const file of files.slice(1)){
+        // console.log(file)
+        console.log(`Processing ${file}`)
+        const command = `ffmpeg -i ${vidPath}/${file} -r 1 -qscale:v 2 ${vidPath}/output/${file}-frame%02d.jpg`
+        execSync(command)
+        console.log(`Done processing ${file}`)
+    }
+}
+
+async function main(){
+    const info = await ytdl.getInfo("https://www.youtube.com/watch?v=S9fiZuwHCVY")
+    if (await recordExistInFirestore(info.videoDetails.videoId)) {
+        console.log("Record already exists")
+        return
+    }
+    await downloadFromYoutube(info)
+    await segmentVideo(info)
+    await frames(info)
+    await uploadToFirebase(`temp/${info.videoDetails.videoId}/output`, info.videoDetails.videoId)
+    await recordInFirestore(info.videoDetails.videoId)
+}
+main().then()
